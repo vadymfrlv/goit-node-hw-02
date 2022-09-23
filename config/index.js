33382c7a1860c24f7506919d1b1021e5e9ecdca9
Vadym Frolov <vdfroloff@gmail.com>
2022-09-23 13:51:50 +0300

    upds

--- config/index.js
+++ config/index.js
@@ -1,10 +1,9 @@
 const ENV = process.env.NODE_ENV || 'dev';
-
 require('dotenv').config({ path: `.env.${ENV}` });
 
-const { MONGO_URL, PORT } = process.env;
+const { MONGO_URI, PORT } = process.env;
 
 module.exports = {
-  port: Number(PORT) || 5000,
-  mongoURL: MONGO_URL,
+  port: Number(PORT) || 3000,
+  mongoURI: MONGO_URI,
 };
